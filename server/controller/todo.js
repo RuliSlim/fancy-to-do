6f58760296770db21ef5b6759cf46c2d3fefb9d8
RuliSlim <ruli0to8@gmail.com>
2020-03-30 16:08:10 +0700

    selesai

--- server/controller/todo.js
+++ server/controller/todo.js
@@ -2,49 +2,52 @@ const {Todo} = require('../models')
 
 class TodoController {
   static getAll(req, res) {
-    console.log('Todo All')
     Todo.findAll()
       .then((todos) => {
-        res.status(200).json({todos})
+        res.status(200).json(todos)
       })
       .catch((err) => {
-        res.status(500).json({err})
+        res.status(500).json(err)
       })
   }
 
   static getOne(req, res) {
-    console.log('Todo One')
     Todo.findOne({where: {id: req.params.id}})
       .then((todo) => {
-        res.status(200).json({todo});
+        res.status(200).json(todo);
       })
       .catch((err) => {
-        res.status(404).json({err});
+        res.status(404).json(err);
       })
   }
   
   static create(req, res) {
-    console.log('Todo create')
     const {title, description, due_date} = req.body
     Todo.create({
       title,
       description,
       due_date
     })
-      .then(todo => [
+      .then(todo => {
         res.status(201).json(todo)
-      ])
+      })
+      .catch(err => {
+        if (err.name === 'SequelizeValidationError') {
+          res.status(400).json(err)
+        } else {
+          res.status(500).json(err)
+        }
+      })
   }
 
-  static updateOne(req, res) {
-    console.log('Todo Update')
+  static updateAll(req, res) {
     const {title, description, due_date, status} = req.body
     Todo.update(
       {
-        title,
-        description,
-        due_date,
-        status
+        title: title,
+        description: description || null,
+        due_date: due_date|| null,
+        status: status || false
       },
       {
         where: {id: req.params.id},
@@ -52,12 +55,62 @@ class TodoController {
       }
       )
         .then(todo => {
-          console.log(todo)
-          res.status(200).json({todo})
+          if (todo) {
+            res.status(200).json(todo)
+          } else {
+            res.status(404).json({message: 'Todo not found'})
+          }
         })
         .catch(err => {
-          res.status(500).json({err})
+          if (err.name === 'SequelizeValidationError') {
+            res.status(400).json(err)
+          } else {
+            res.status(500).json(err)
+          }
         })
+  }
+
+  static updateOne(req, res) {
+    const {id, key} = req.params
+    const reqBody = req.body
+    Todo.findByPk(id)
+      .then(todo => {
+        Object.keys(todo.dataValues).forEach(el => {
+          if (key === el) {
+            return Todo.update(
+              reqBody,
+              {
+                where: {id: req.params.id},
+                returning: true
+              }
+            )
+          } else {
+            return res.status(404).json({message: 'Not Found property with name ' + key})
+          }
+        })
+      })
+      .then(todo => {
+        // console.log(todo, 'IKI') 
+        // Ini kenapa ga me-return hasil todo yang baru di update ya? padahal udah dikasih option returning true
+        res.status(200).json(todo)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+  static deleteOne(req, res) {
+    Todo.destroy({where: {id: req.params.id}, returning: true})
+      .then(todo => {
+        if (todo) {
+          res.status(200).json(todo)
+        } else {
+          res.status(404).json({message: 'Todo not found'})
+        }
+      })
+      .catch(err  => {
+        res.status(500).json(err)
+      })
   }
 }
 
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -4,7 +4,8 @@ const {TodoController} = require('../controller')
 router.get('/', TodoController.getAll)
 router.post('/', TodoController.create)
 router.get('/:id', TodoController.getOne)
-router.put('/:id', TodoController.updateOne)
-
+router.put('/:id', TodoController.updateAll)
+router.patch('/:id/:key', TodoController.updateOne)
+router.delete('/:id', TodoController.deleteOne)
 
 module.exports = router
